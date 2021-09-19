5a1aa8c9320e4c5957077a832c1fd14a0d0697c7
nekomaru0510 <yama05rymy@gmail.com>
2021-09-19 01:24:37 +0000

    refactoring now ...

--- violet/class/src/irq.rs
+++
@@ -1,6 +0,0 @@
-pub trait IrqAttr {
-    fn new() -> Self;
-    fn enable_interrupt(&self, id: u64);
-    //fn disable_interrupt(&self, id: u64);
-}
-
--- violet/class/src/timer.rs
+++
@@ -1,8 +0,0 @@
-pub trait TimerAttr {
-    fn new() -> Self;
-    fn write(&self, t: u64);
-    fn read(&self) -> u64;
-    fn enable_interrupt(&self);
-    fn disable_interrupt(&self);
-    fn set_interrupt_time(&self, t: u64); // いるかどうか要検討
-}
