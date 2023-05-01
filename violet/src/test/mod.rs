3d62e681ae6ff0979f9e8c0e2d34af0dac1d8196
nekomaru_0510 <yama05rymy@gmail.com>
2023-05-01 14:07:57 +0000

    fix test module

--- violet/src/test/mod.rs
+++ violet/src/test/mod.rs
@@ -1,16 +1,32 @@
 //! Test関連
+extern crate core;
+use core::intrinsics::transmute;
 use crate::{print, println};
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Fn() -> Result<(), &'static str>]) {
+    let mut success = 0;
     println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        match test() {
+            Ok(()) => {
+                println!("[ok]");
+                success += 1;
+            },
+            Err(e) => {
+                let addr: usize = unsafe {transmute(test)};
+                println!("[ng] Function:0x{:x} ... {}", addr, e);
+            }
+        }
     }
+    println!("Result: {}/{} ", success, tests.len());
 }
 
 #[test_case]
-fn test_test() {
-    assert_eq!(1, 1);
-    println!("[ok]");
+fn test_test() -> Result<(), &'static str> {
+    if 1 == 1 {
+        Ok(())
+    } else {
+        Err("error")
+    }
 }
